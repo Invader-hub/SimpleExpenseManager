46bd9b39f33f06a547e2caeb32e77917e752342d
Achira Dias <achira828@gmail.com>
2022-04-30 23:17:22 +0530

    Add concrete classes

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -0,0 +1,13 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+
+public class PersistentExpenseManager extends ExpenseManager{
+
+    public  PersistentExpenseManager() throws ExpenseManagerException {setup();}
+
+    @Override
+    public void setup() throws ExpenseManagerException {
+
+    }
+}
