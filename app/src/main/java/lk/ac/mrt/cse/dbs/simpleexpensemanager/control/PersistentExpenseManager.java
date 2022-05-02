776a36b6656106b24eed060d95b5360de91fe21d
Achira Dias <achira828@gmail.com>
2022-05-02 05:55:30 +0530

    Update DataBaseHelper.java

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -1,6 +1,13 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
 
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.DataBaseHelper;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.InMemoryAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.InMemoryTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.ui.MainActivity;
 
 public class PersistentExpenseManager extends ExpenseManager{
 
@@ -8,6 +15,22 @@ public class PersistentExpenseManager extends ExpenseManager{
 
     @Override
     public void setup() throws ExpenseManagerException {
+        /*** Begin generating dummy data for In-Memory implementation ***/
+//        DataBaseHelper dbh;
+//        dbh = new DataBaseHelper(MainActivity.this);
 
+        TransactionDAO inMemoryTransactionDAO = new InMemoryTransactionDAO();
+        setTransactionsDAO(inMemoryTransactionDAO);
+
+        AccountDAO inMemoryAccountDAO = new InMemoryAccountDAO();
+        setAccountsDAO(inMemoryAccountDAO);
+
+        // dummy data
+        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+        getAccountsDAO().addAccount(dummyAcct1);
+        getAccountsDAO().addAccount(dummyAcct2);
+
+        /*** End ***/
     }
 }
--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/DataBaseHelper.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/DataBaseHelper.java
@@ -2,11 +2,20 @@ package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
@@ -16,7 +25,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     private static final String BANK_NAME = "bank_name";
     private static final String ACCOUNT_HOLDER = "account_holder";
     private static final String INITIAL_BALANCE = "initial_balance";
-    private static final String LOG = "log";
+    private static final String TRANSACTION_LOG = "transaction_log";
     private static final String ID = "id";
     private static final String DATE = "date";
     private static final String TRANSACTION_TYPE = "transaction_type";
@@ -30,7 +39,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTableStatement1 = "CREATE TABLE " + ACCOUNT + " (" + ACCOUNT_NO + " VARCHAR(50) PRIMARY KEY, " + BANK_NAME + " VARCHAR(100), " + ACCOUNT_HOLDER + " VARCHAR(100), " + INITIAL_BALANCE + " FLOAT);";
-        String createTableStatement2 = "CREATE TABLE " + LOG + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + DATE + " DATE, " + ACCOUNT_NO+ " VARCHAR(50)," + TRANSACTION_TYPE + " INT, " + AMOUNT + " FLOAT);";
+        String createTableStatement2 = "CREATE TABLE " + TRANSACTION_LOG + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + DATE + " DATE, " + ACCOUNT_NO+ " VARCHAR(50)," + TRANSACTION_TYPE + " INT, " + AMOUNT + " FLOAT);";
 
         db.execSQL(createTableStatement1);
         db.execSQL(createTableStatement2);
@@ -43,7 +52,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     }
 
-    public boolean addOne(Account account){
+    public boolean addOneAccount(Account account){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
 
@@ -59,6 +68,110 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         }
         else {
         return true;
-        4}
+        }
     }
+
+    public boolean addOneTransactionLog(Transaction transaction){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        // Formatting date for db insert
+        Date date = transaction.getDate();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String datestr = formatter.format(date);
+
+        // Formatting transaction type for db insert
+        Integer transaction_type_value = 0;
+        ExpenseType transaction_type = transaction.getExpenseType();
+        if (transaction_type == ExpenseType.EXPENSE){
+            transaction_type_value = 1;
+        }
+        else if (transaction_type == ExpenseType.INCOME){
+            transaction_type_value = 2;
+        }
+
+        cv.put(DATE, datestr);
+        cv.put(ACCOUNT_NO, transaction.getAccountNo());
+        cv.put(TRANSACTION_TYPE, transaction_type_value);
+        cv.put(AMOUNT, transaction.getAmount());
+
+        long insert = db.insert(TRANSACTION_LOG, null, cv);
+
+        if (insert == -1){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    
+    public List<Transaction> getAllLog(){
+        List<Transaction> returnList = new ArrayList<>();
+
+        String sql_query = "SELECT * FROM "+ TRANSACTION_LOG;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql_query, null);
+
+        if(cursor.moveToFirst()){
+            do {
+                int transactionID = cursor.getInt(0);
+                String datestr = cursor.getString(1);
+                String accountNo = cursor.getString(2);
+                int transactionType = cursor.getInt(3);
+                float amount = cursor.getFloat(4);
+
+                Date date = null;
+                try {
+                    date = new SimpleDateFormat("yyyy-MM-dd").parse(datestr);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                ExpenseType ex = null;
+                if(transactionType == 1){
+                    ex = ExpenseType.EXPENSE;
+                }
+                else if (transactionType == 2){
+                    ex = ExpenseType.INCOME;
+                }
+
+                Transaction curTransaction = new Transaction(date, accountNo, ex, amount);
+                returnList.add(curTransaction);
+
+            }while (cursor.moveToNext());
+
+        }else {
+            // Do nothing;
+        }
+
+        return returnList;
+    }
+
+    public List<Account> getAllAccount(){
+        List<Account> returnList = new ArrayList<>();
+
+        String sql_query = "SELECT * FROM "+ ACCOUNT;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql_query, null);
+
+        if(cursor.moveToFirst()){
+            do {
+                String accountNo = cursor.getString(0);
+                String bankName = cursor.getString(1);
+                String accountHolder = cursor.getString(2);
+                float initialBalance = cursor.getFloat(3);
+
+                Account curAccount = new Account(accountNo, bankName, accountHolder, initialBalance);
+
+                returnList.add(curAccount);
+
+            }while (cursor.moveToNext());
+
+        }else {
+            // Do nothing;
+        }
+
+        return returnList;
+    }
+
 }
