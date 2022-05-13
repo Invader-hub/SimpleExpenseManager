7219b8177a466e37136eef3f9cce7fa091b976b4
Achira Dias <achira828@gmail.com>
2022-05-13 20:21:08 +0530

    Update ApplicationTest.java

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -48,28 +48,45 @@ public class ApplicationTest {
     @Test
     public void testAddAccount() {
         expenseManager.addAccount("1234", "AAA", "BBB", 200.0);
+        expenseManager.addAccount("190137J", "BOC", "Name", 20000.0);
         List<String> accountNumbers = expenseManager.getAccountNumbersList();
         assertTrue(accountNumbers.contains("1234"));
+        assertTrue(accountNumbers.contains("190137J"));
     }
 
     @Test
     public void testAddTransaction() {
         try {
             expenseManager.updateAccountBalance("1234", 11, 04, 2022, ExpenseType.EXPENSE, "500.0");
+            expenseManager.updateAccountBalance("190137J", 10, 02, 1999, ExpenseType.INCOME, "9999.57");
         } catch (InvalidAccountException e) {
             assertTrue(false);
         }
         List<Transaction> transactions = expenseManager.getTransactionLogs();
         for (Transaction t : transactions) {
+            if (t.getAccountNo().equals("1234")) {
+                assertTrue(t.getAccountNo().equals("1234"));
+                assertTrue(t.getAmount() == 500.0);
 
-            assertTrue(t.getAccountNo().equals("1234"));
-            assertTrue(t.getAmount() == 500.0);
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                String strDate = dateFormat.format(t.getDate());
+                assertTrue(strDate.equals("2022-05-11"));
+                assertTrue(t.getExpenseType() == ExpenseType.EXPENSE);
 
-            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            String strDate = dateFormat.format(t.getDate());
-            assertTrue(strDate.equals("2022-05-11"));
-            assertTrue(t.getExpenseType() == ExpenseType.EXPENSE);
+            }
+            else if (t.getAccountNo().equals("190137J")) {
+                assertTrue(t.getAccountNo().equals("190137J"));
+                assertTrue(t.getAmount() == 9999.57);
 
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                String strDate = dateFormat.format(t.getDate());
+                assertTrue(strDate.equals("1999-03-10"));
+                assertTrue(t.getExpenseType() == ExpenseType.INCOME);
+
+            }
+            else {
+                assertTrue(false);
+            }
         }
     }
 }
